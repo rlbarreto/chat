fbb528265e81bee885b9fd21fc67fbac1b8d4fef
Rafael Lessa Barreto <rafael@navesbarreto.com>
2016-06-26 22:42:01 -0300

    Websocket created. Creating multiple rooms

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -23,7 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 
 app.use('/', routes);
