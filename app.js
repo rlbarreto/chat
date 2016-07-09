bb1a2def61ee5741c40316f13bf9368a7f7aeb46
Rafael Lessa Barreto <rafael@navesbarreto.com>
2016-07-09 16:29:09 -0300

    Mongoose warning message about promises resolved

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-var app = express();
+const routes = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, '/views'));
